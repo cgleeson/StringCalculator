e1607a819f0a92faeac92e2ab05808d14495084a
CAthal Gleeson <cathal.gleeson@gmail.com>
2015-05-25 15:33:35 +0100

    Initial Commit

---
+++ src/stringcalculator/StringCalculator.java
@@ -0,0 +1,9 @@
+package stringcalculator;
+
+public class StringCalculator {
+
+	public int add(int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ src/teststringcalculator/TestStringCalculator.java
@@ -0,0 +1,19 @@
+package teststringcalculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import stringcalculator.StringCalculator;
+
+public class TestStringCalculator {
+	
+	StringCalculator cal = new StringCalculator();
+
+	@Test
+	public void add() {
+		
+		assertEquals (cal.add(10, 20), 30) ;
+	}
+
+}
